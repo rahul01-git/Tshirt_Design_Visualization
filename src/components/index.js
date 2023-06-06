83990b6080295fe91054bc2e670a60dc73de0489
rahul01-git <rahulneupane0090@gmail.com>
2023-06-06 19:33:17 +0545

    customizer

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,7 @@
 import CustomButton from "./CustomButton";
+import AIPicker from "./AIPicker";
+import FilePicker from "./FilePicker";
+import ColorPicker from "./ColorPicker";
+import Tab from "./Tab";
 
-export { CustomButton }
+export { CustomButton, AIPicker, ColorPicker, FilePicker, Tab }
