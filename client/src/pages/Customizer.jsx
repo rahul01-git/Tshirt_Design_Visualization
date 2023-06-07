b4fa0f1c959bead8eb21b14366063e4649f92da6
rahul01-git <rahulneupane0090@gmail.com>
2023-06-07 15:35:44 +0545

    ai dalle picker done

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -39,35 +39,51 @@ const Customizer = () => {
         return <FilePicker file={file} setFile={setFile} readFile={readFile} />;
 
       case "aipicker":
-        return <AIPicker 
-        prompt={prompt}
-        setPrompt={setPrompt}
-        generatingImg={generatingImg}
-        handleSubmit={handleSubmit}
-        />;
+        return (
+          <AIPicker
+            prompt={prompt}
+            setPrompt={setPrompt}
+            generatingImg={generatingImg}
+            handleSubmit={handleSubmit}
+          />
+        );
       default:
         return null;
     }
   };
 
   const handleSubmit = async (type) => {
-    if(!prompt) return alert("Please enter a prompt")
+    if (!prompt) return alert("Please enter a prompt");
 
-    try{
-        //call our backend to generate an ai image
-    }catch(error){
-      alert(error)
-    }finally{
-      setGeneratingImg(false)
-      setActiveEditorTab("")
+    try {
+      setGeneratingImg(true);
+      const response = await fetch("http://localhost:8080/api/v1/dalle", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          prompt,
+        }),
+      });
+      const data = await response.json();
+
+      handleDecals(type, `data:image/png;base64,${data.photo}`);
+    } catch (error) {
+      alert(error);
+    } finally {
+      setGeneratingImg(false);
+      setActiveEditorTab("");
     }
-  }
+  };
   const handleDecals = (type, result) => {
     const decalType = DecalTypes[type];
     state[decalType.stateProperty] = result;
 
     if (!activeFilterTab[decalType.filterTab]) {
       handleActiveFilterTab(decalType.filterTab);
+    }else{
+      console.error("Decal result is undefined")
     }
   };
 
