9947821256a53cbb7525417e6901e7fe6e4a5145
rahul01-git <rahulneupane0090@gmail.com>
2023-06-07 13:24:43 +0545

    colour picker implemented

--- src/components/ColorPicker.jsx
+++ src/components/ColorPicker.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { SketchPicker } from "react-color";
+import { useSnapshot } from "valtio";
 
+import state from "../store";
 const ColorPicker = () => {
+  const snap = useSnapshot(state);
   return (
-    <div>ColorPicker</div>
-  )
-}
+    <div className="absolute left-full ml-3">
+      <SketchPicker
+        color={snap.color}
+        onChange={(color) => (state.color = color.hex)}
+        disableAlpha
+      />
+    </div>
+  );
+};
 
-export default ColorPicker
+export default ColorPicker;
